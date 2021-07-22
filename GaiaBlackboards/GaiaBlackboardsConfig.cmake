f034e727cc46f418cfb8e33b848eecad39f63370
Vincent <rumble.vecent@outlook.com>
2021-07-22 20:50:01 +0800

    Change install script, add version support.

--- GaiaBlackboards/GaiaBlackboardsConfig.cmake
+++
@@ -1,8 +0,0 @@
-cmake_minimum_required(3.10)
-
-find_path(GaiaBlackboards_INCLUDE_DIRS "GaiaBlackboards")
-find_library(GaiaBlackboards_LIBS "GaiaBlackboards")
-
-if (GaiaBlackboards_INCLUDE_DIRS AND GaiaBlackboards_LIBS)
-    set(GaiaBlackboards_FOUND TRUE)
-endif()
